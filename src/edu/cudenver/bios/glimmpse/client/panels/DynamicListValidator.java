6d69c6697a35ff86b0c2890395b794fecd328eb7
Sarah Kreidler <sarah5280@gmail.com>
2011-05-16 00:49:30 +0000

    removed unused list panel

--- src/edu/cudenver/bios/glimmpse/client/panels/DynamicListValidator.java
+++
@@ -1,8 +0,0 @@
-package edu.cudenver.bios.glimmpse.client.panels;
-
-public interface DynamicListValidator
-{
-	public void validate(String value, int column) throws IllegalArgumentException;
-	
-	public void onValidRowCount(int validRowCount);
-}
